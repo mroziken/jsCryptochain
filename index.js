6bd5204c9f2caee9cd94191db4c2ea144865a061
mroziken <mroziken@users.noreply.github.com>
2022-12-28 20:37:08 +0100

    project structure reorg part 2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const request = require('request');
 const bodyParser = require('body-parser');
 const Blockchain = require('./blockchain');
-const PubSub = require('./pubsub.js');
+const PubSub = require('./app/pubsub.js');
 
 
 const app = express();
